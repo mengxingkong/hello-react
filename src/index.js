0199540dfc72daed429bde4485a376589a3b2f46
p_ljzzhang(张利军) <p_ljzzhang@tencent.com>
2019-01-03 14:23:53 +0800

    just learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class LikeButton extends Component {
+  static defaultProps = {
+    likeText: '取消',
+    unlikeText: '点赞'
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor () {
+    super()
+    this.state = { isLiked: false }
+  }
+
+  handleClickOnLikeButton () {
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
+  }
+
+  render () {
+    return (
+      <button onClick={this.handleClickOnLikeButton.bind(this)}>
+        {this.state.isLiked
+          ? this.props.likeText
+          : this.props.unlikeText} 👍
+      </button>
+    )
+  }
+}
+
+const users = [
+  { username: 'Jerry', age: 21, gender: 'male' },
+  { username: 'Tomy', age: 22, gender: 'male' },
+  { username: 'Lily', age: 19, gender: 'female' },
+  { username: 'Lucy', age: 20, gender: 'female' }
+]
+
+class User extends Component{
+  render(){
+    const { user } = this.props
+    return (
+      <div>
+        <div>姓名：{user.username}</div>
+        <div>年龄：{user.age}</div>
+        <div>性别：{user.gender}</div>
+        <hr />
+      </div>
+    )
+  }
+}
+
+class Index extends Component{
+  render(){
+    return(
+      <div>
+        {users.map( (user,i) =>  <User key={i} user={user} /> )}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root')
+) 
